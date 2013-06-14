5d107a9b3fb9ad2db3f1b593e4beb9b53daefa1c
automation <inhae.kim@falconstor.com>
2013-06-13 23:08:37 -0400

    following the tutorial

--- scrap/ScrapProject/Scrap/src/main/java/com/lp25/scrap/MainActivity.java
+++ scrap/ScrapProject/Scrap/src/main/java/com/lp25/scrap/MainActivity.java
@@ -1,8 +1,11 @@
 package com.lp25.scrap;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.app.Activity;
 import android.view.Menu;
+import android.view.View;
+import android.widget.EditText;
 
 public class MainActivity extends Activity {
 
@@ -19,5 +22,11 @@ public class MainActivity extends Activity {
         getMenuInflater().inflate(R.menu.main, menu);
         return true;
     }
-    
+
+    public void sendMessage(View view) {
+        Intent intent = new Intent(this, DisplayMessageActivity.class);
+        EditText editText = (EditText) findViewById(R.id.edit_message);
+        String message = editText.getText().toString();
+        intent.putExtra(EXTRA_MESSAGE, message);
+    }
 }
